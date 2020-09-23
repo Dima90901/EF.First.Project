d2d4b5f4f6c664bc008ab1b4a68eec48fc40461e
Dima90901 <pasichnyi.dima@gmail.com>
2020-09-23 15:21:59 +0300

    Add project files.

---
+++ EF.First.Project.BLL/Services/FirstBLLServiceCars.cs
@@ -0,0 +1,33 @@
+﻿using EF.First.Project.DAL.Contex;
+using EF.First.Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.First.Project.BLL.Services
+{
+    public class FirstBLLServiceCars : IDisposable
+    {
+        private readonly Model1 db = new Model1();
+        public IEnumerable<MyFirsModelCars> GetAllCars()
+        {
+            return db.MyFirstCars;
+        }
+        public void AddCars ()
+        {
+            MyFirsModelCars cars = new MyFirsModelCars
+            {
+                Id = 1,
+                Name = "Bently"
+            };
+            db.MyFirstCars.Add(cars);
+            db.SaveChanges();
+        }
+        public void Dispose ()
+        {
+            db.Dispose();
+        }
+    }
+}
---
+++ EF.First.Project.PL.Console/Program.cs
@@ -0,0 +1,24 @@
+﻿using EF.First.Project.BLL.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.First.Project.PL.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            FirstBLLServiceCars db = new FirstBLLServiceCars();
+            db.AddCars();
+            var cars = db.GetAllCars();
+            foreach (var item in cars)
+            {
+                System.Console.WriteLine(item.Id + " - " + item.Name);
+            }
+            System.Console.ReadKey();
+        }
+    }
+}
