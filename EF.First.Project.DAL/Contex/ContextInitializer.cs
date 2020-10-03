b212e3d374705dc14f32c2c04a2f6538d938aa8f
Dima90901 <pasichnyi.dima@gmail.com>
2020-10-03 17:15:14 +0300

    Added Initializer

---
+++ EF.First.Project.DAL/Contex/ContextInitializer.cs
@@ -0,0 +1,69 @@
+﻿using EF.First.Project.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EF.First.Project.DAL.Contex
+{
+    class ContextInitializer : DropCreateDatabaseIfModelChanges<Model1>
+    {
+        protected override void Seed(Model1 context)
+        {
+            base.Seed(context);
+            Audience audiences = new Audience()
+            {
+                NumberAudience = 1,
+                TimeForLessons = new DateTime(2020, 10, 2, 19, 00, 00)
+            };
+            context.Audiences.Add(audiences);
+            Group group = new Group()
+            {
+                CountOfStudents = 3,
+                GroupName = ".NET dev",
+                IdSpeciality = 1,
+                TrainerId = 1,
+            };
+            context.Groups.Add(group);
+            Speciality speciality = new Speciality()
+            {
+                NameSpeciality = ".NET fullstack developer",
+                TimeForStudyInMonth = 40
+            };
+            context.Specialities.Add(speciality);
+            List<Student> students = new List<Student>()
+            {
+                new Student
+                {
+                    GroupId = 1,
+                    StudentAge = 30,
+                    StudentFName = "Dima",
+                    StudentLName = "Pasichnyi"
+                },
+                new Student
+                {
+                    GroupId = 1,
+                    StudentAge = 30,
+                    StudentFName = "Anton",
+                    StudentLName = "Byrko"
+                },
+                new Student
+                {
+                    GroupId = 1,
+                    StudentAge = 30,
+                    StudentFName = "Sherhan",
+                    StudentLName = "Tagubaev"
+                }
+            };
+            context.Students.AddRange(students);
+            Trainer trainer = new Trainer()
+            {
+                ExpWorkInYears = 5,
+                Name = "Roman"
+            };
+            context.Trainers.Add(trainer);
+        }
+    }
+}
